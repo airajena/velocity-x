ebb126b293e3ce27cb344af43227d73519b9030b
airajena <airajena.hack@gmail.com>
2025-12-07 19:00:19 +0530

    config: configure redis caching with custom ttl

---
+++ transaction-service/src/main/java/com/velocityx/transaction_service/config/RedisCacheConfig.java
@@ -0,0 +1,47 @@
+package com.velocityx.transaction_service.config;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+
+import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+@EnableCaching
+public class RedisCacheConfig {
+    
+    @Bean
+    public RedisCacheManager cacheManager(RedisConnectionFactory connectionFactory) {
+        RedisCacheConfiguration defaultConfig = RedisCacheConfiguration
+                .defaultCacheConfig()
+                .entryTtl(Duration.ofMinutes(15))
+                .serializeValuesWith(
+                        RedisSerializationContext.SerializationPair.fromSerializer(
+                                new GenericJackson2JsonRedisSerializer()
+                        )
+                );
+        
+        Map<String, RedisCacheConfiguration> cacheConfigurations = new HashMap<>();
+        
+        cacheConfigurations.put("transactions", 
+                defaultConfig.entryTtl(Duration.ofMinutes(15)));
+        
+        cacheConfigurations.put("transactionsByUser", 
+                defaultConfig.entryTtl(Duration.ofMinutes(10)));
+        
+        cacheConfigurations.put("idempotencyKeys", 
+                defaultConfig.entryTtl(Duration.ofHours(24)));
+        
+        return RedisCacheManager.builder(connectionFactory)
+                .cacheDefaults(defaultConfig)
+                .withInitialCacheConfigurations(cacheConfigurations)
+                .build();
+    }
+}
