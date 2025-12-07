129f8084bb5edd75f8a65912fc2ba3cc779eda71
airajena <airajena.hack@gmail.com>
2025-12-07 19:00:03 +0530

    feat: add mapstruct mapper for entity-dto conversion

---
+++ transaction-service/src/main/java/com/velocityx/transaction_service/mapper/TransactionMapper.java
@@ -0,0 +1,37 @@
+package com.velocityx.transaction_service.mapper;
+
+import com.velocityx.transaction_service.dto.request.CreateTransactionRequest;
+import com.velocityx.transaction_service.dto.response.TransactionResponse;
+import com.velocityx.transaction_service.entity.Transaction;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface TransactionMapper {
+    
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "transactionId", ignore = true)
+    @Mapping(target = "status", ignore = true)
+    @Mapping(target = "paymentGatewayRef", ignore = true)
+    @Mapping(target = "failureReason", ignore = true)
+    @Mapping(target = "parentTransactionId", ignore = true)
+    @Mapping(target = "holdExpiresAt", ignore = true)
+    @Mapping(target = "capturedAt", ignore = true)
+    @Mapping(target = "refundedAt", ignore = true)
+    @Mapping(target = "cancelledAt", ignore = true)
+    @Mapping(target = "processingStartedAt", ignore = true)
+    @Mapping(target = "completedAt", ignore = true)
+    @Mapping(target = "events", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "updatedAt", ignore = true)
+    @Mapping(target = "createdBy", ignore = true)
+    @Mapping(target = "updatedBy", ignore = true)
+    @Mapping(target = "version", ignore = true)
+    Transaction toEntity(CreateTransactionRequest request);
+    
+    TransactionResponse toDto(Transaction transaction);
+    
+    List<TransactionResponse> toDtoList(List<Transaction> transactions);
+}
