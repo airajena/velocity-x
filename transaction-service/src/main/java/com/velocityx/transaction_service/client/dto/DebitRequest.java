92c1eed32deaf74fbc5fab8b3d99212910ec3b3c
airajena <airajena.hack@gmail.com>
2025-12-07 19:00:51 +0530

    feat: add wallet service client dtos

---
+++ transaction-service/src/main/java/com/velocityx/transaction_service/client/dto/DebitRequest.java
@@ -0,0 +1,20 @@
+package com.velocityx.transaction_service.client.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class DebitRequest {
+    private Long walletId;
+    private BigDecimal amount;
+    private String currency;
+    private String transactionId;
+    private String description;
+}
