79fec0b61e70817303acc365819b63ccf339c89d
airajena <airajena.hack@gmail.com>
2025-12-12 16:48:52 +0530

    chore: add dockerfile and test infrastructure

---
+++ fraud-detection-service/src/test/java/com/velocityx/fraud_detection_service/FraudDetectionServiceApplicationTests.java
@@ -0,0 +1,12 @@
+package com.velocityx.fraud_detection_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FraudDetectionServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
